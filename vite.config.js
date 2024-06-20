99db30ba25b67872f7f0dcfa40e01d3227dd217e
victouriousprincess <victouriousprincess725@gmail.com>
2024-06-21 02:41:00 +0800

    change config

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: '127.0.0.1',
-//     port: 3030,
-//     watch: {
-//       usePolling: true,
-//       interval: 100,
-//     },
-//   },
-// });
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-  if (command === 'serve') {
-    return {
-      // dev specific config
-      plugins: [react()],
-      server: {
-        host: '0.0.0.0',
-        port: 3030,
-        watch: {
-          usePolling: true,
-          interval: 100,
-        },
-      },
-    }
-  } else {
-    // command === 'build'
-    return {
-      // build specific config
-      // base: '/victouriousprincess/',
-      plugins: [react()]
-    }
-  }
-})
+export default defineConfig({
+  plugins: [react()],
+  base: 'bridge'
+});
+// export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+//   if (command === 'serve') {
+//     return {
+//       // dev specific config
+//       plugins: [react()],
+//       server: {
+//         host: '0.0.0.0',
+//         port: 3030,
+//         watch: {
+//           usePolling: true,
+//           interval: 100,
+//         },
+//       },
+//     }
+//   } else {
+//     // command === 'build'
+//     return {
+//       // build specific config
+//       // base: '/victouriousprincess/',
+//       plugins: [react()]
+//     }
+//   }
+// })
