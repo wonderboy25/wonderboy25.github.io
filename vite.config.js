a39a71974c3e73b2ee8ecc960159252b3914f11a
victouriousprincess <victouriousprincess725@gmail.com>
2024-06-21 08:38:28 +0800

    set

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/wonderboy25/'
+  plugins: [react()]
 });
 // export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
 //   if (command === 'serve') {
